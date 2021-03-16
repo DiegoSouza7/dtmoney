65d1db6a31fbede21aa67a3845c4e88a34e3c829
DiegoSouza7 <diego78523@gmail.com>
2021-03-16 12:40:18 -0300

    componentization

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,7 @@ import {createGlobalStyle} from 'styled-components'
 export const GlobalStyles = createGlobalStyle`
   :root {
     --background: #f0f2f5;
+    --green: #33CC95;
     --red: #E52E4D;
     --blue: #5429CC;
     --blue-light: #6933FF;
