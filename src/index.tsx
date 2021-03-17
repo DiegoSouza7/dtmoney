515eea375fdf4ab3ec2c8bdbbceeaaa34e721485
DiegoSouza7 <diego78523@gmail.com>
2021-03-17 14:18:12 -0300

    modal and forms

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,45 @@
+import { createServer, Model } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs'
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 10000,
+          createdAt: new Date('2021-02-15 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Computador',
+          type: 'withdraw',
+          category: 'Trabalho',
+          amount: 5000,
+          createdAt: new Date('2021-02-18 14:00:00')
+        }
+      ]
+    })
+  },
   routes() {
     this.namespace = 'api'
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
     })
   }
 })
