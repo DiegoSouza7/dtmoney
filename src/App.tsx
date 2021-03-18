e586d79083bf62921fc4ca8d79bfa6d19aaf6606
DiegoSouza7 <diego78523@gmail.com>
2021-03-18 14:24:08 -0300

    context and hooks

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyles } from "./styles/global";
+import { TransactionProvider } from "./hooks/useTransactions";
 
 Modal.setAppElement('#root')
 
@@ -18,7 +19,7 @@ export function App() {
     setIsNewTransactionModalOpen(false)
   }
   return (
-    <>
+    <TransactionProvider>
      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
      <Dashboard/>
       <NewTransactionModal
@@ -26,6 +27,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
      <GlobalStyles />
-    </>
+    </TransactionProvider>
   );
 }
